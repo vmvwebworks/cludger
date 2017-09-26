84d76947bf229d4247499ef3df7f9064f934ddda
Marcos <vmvwebworks@gmail.com>
2017-09-26 15:19:21 +0200

    action cable retransmite por pantalla lo que se envia por consola a los subscriptores que entran al canal

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'rooms/show'
+  #get 'rooms/show'
+  root to: 'rooms#show'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions', registrations: 'users/registrations' }
-  get '/', to: 'home#index' #:as => "root"
+  #get '/', to: 'home#index' #:as => "root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount ActionCable.server => '/cable'
 end
