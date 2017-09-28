33ea50fc78806a6fe64f618b34a7b3550c146e6d
Marcos <vmvwebworks@gmail.com>
2017-09-28 23:58:43 +0200

    cambiado root a default de nuevo y input en shows para añadir contenido

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   #get 'rooms/show'
-  root to: 'rooms#show'
-
+  root to: 'home#index'
+  get 'rooms/show', to: 'rooms#show'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions', registrations: 'users/registrations' }
-  #get '/', to: 'home#index' #:as => "root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount ActionCable.server => '/cable'
 end
