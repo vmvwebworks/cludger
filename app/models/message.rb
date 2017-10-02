9c7246154174218c93b5cea410700adc23c9bc77
Marcos <vmvwebworks@gmail.com>
2017-10-02 10:59:20 +0200

    jobs con actioncable

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,11 +1,11 @@
 class Message
   extend ActiveJob
   include Mongoid::Document
+  include GlobalID::Identification
   field :content, type: String
   after_create do |document|
-    puts "esto lo hace"
-    document = document.to_json
-    puts document
+    puts "pasa por el modelo message"
+  
     MessageBroadcastJob.perform_later(document)
   end
 end
