f1142e564bf7d930c657b009874375889eaae04b
Marcos <vmvwebworks@gmail.com>
2017-10-07 19:32:00 +0200

    correccion sesiones

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,22 @@ class User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :timeoutable, omniauth_providers: [:google_oauth2]
   def self.from_omniauth(access_token)
     data = access_token.info
-    puts data['email']
-    where(provider: data['provider'], uid: data['uid']).first_or_create do |user|
+    where(provider: access_token['provider'], uid: access_token['uid']).first_or_create do |user|
       user.name = data['first_name']
       user.last_name = data['last_name']
       user.email = data['email']
       user.email_verified = true
-      user.provider = data['provider']
-      user.uid = data['uid']
+      user.provider = access_token['provider']
+      user.uid = access_token['uid']
       user.profile_image = data['image']
       user.encrypted_password = Devise.friendly_token[0,20]
     end
+  end
+  def contacts
+    User.where(id: contact_list)
   end
   #has_and_belongs_to_many :organizations
   ## Database authenticatable
@@ -27,10 +29,11 @@ class User
   field :email_verified,     type: Boolean, default: false
   field :encrypted_password, type: String, default: ""
   field :provider,           type: String, default: ""
-  field :uid,                type: Integer
+  field :uid,                type: String, default: ""
   field :private,            type: Boolean, default: false
   field :profesional_kind,   type: String, default: ""
-  field :pro_verified,       type: String, default: false 
+  field :pro_verified,       type: String, default: false
+  field :contact_list,       type: Array
 
   ## Recoverable
   field :reset_password_token,   type: String
@@ -56,4 +59,5 @@ class User
   # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
+  #scope :contacts, -> { where(id: self.contact_list) }
 end
