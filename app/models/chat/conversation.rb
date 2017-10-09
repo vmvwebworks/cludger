4c5e4f4c9ba841b0ed229dcdb1a3f3814ec97463
Marcos <vmvwebworks@gmail.com>
2017-10-09 18:59:10 +0200

    metodo participate del modelo Conversation

--- app/models/chat/conversation.rb
+++ app/models/chat/conversation.rb
@@ -4,9 +4,9 @@ class Conversation
   has_and_belongs_to_many :users
   has_many :messages
   has_many :conversation_user_details
-  field :created_at, type: Time, default:
-
-
+  def participate?(user)
+    user_ids.include?(user.id)
+  end
   # def participates(user)
   #   user_id.exists?(user.id)
   # end
